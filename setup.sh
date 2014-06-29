908354a7b733f4fb14200f62bf2b1510d6cf1c9e
Andrew Fecheyr <andrew@bedesign.be>
2014-06-30 00:33:39 +0200

    Inital working ubuntu 1404 template

---
+++ setup.sh
@@ -0,0 +1,7 @@
+sudo apt-get install python-software-properties
+sudo apt-add-repository -y ppa:brightbox/ruby-ng
+sudo apt-get update
+sudo apt-get install -y ruby2.1 ruby2.1-dev
+
+# Libraries needed for typical ruby gems
+sudo apt-get install libsqlite3-dev
